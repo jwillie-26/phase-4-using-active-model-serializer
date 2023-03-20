80b3a965202d51aa422e19ca3cbd6e30ea85531c
Jesse <jessewambu@zetech.ac.ke>
2023-03-20 07:54:00 +0300

    Done

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,11 +6,26 @@ class MoviesController < ApplicationController
     render json: movies
   end
 
+    # def show
+  #   movie = Movie.find(params[:id])
+  #   render json: movie.to_json(only: [:id, :title, :year, :length, :director, :description, :poster_url, :category, :discount, :female_director])
+  # end
+
+  # def show
+  #   movie = Movie.find(params[:id])
+  #   render json: movie.to_json(except: [:created_at, :updated_at])
+  #end
+
   def show
     movie = Movie.find(params[:id])
     render json: movie
   end
 
+  def summary
+    movie = Movie.find(params[:id])
+    render json: movie, serializer: MovieSummarySerializer
+  end
+  
   private
 
   def render_not_found_response
